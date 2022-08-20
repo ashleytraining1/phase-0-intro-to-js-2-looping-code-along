9440a429af0c793f7b757ffc3ea165f99be6fce7
ashley <ashley.oketo@student.moringaschool.com>
2022-08-20 09:41:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const gifts = ["Guadalupe", "Ollie", "Aki"]
+
+let myCard = []
+
+const writeCards = (names , party) => {
+  for (let counter = 0; names.length > counter; counter++){
+const card =`Thank you, ${names[counter]}, for the wonderful ${party} gift!`
+
+myCard.push(card)
+
+  }return myCard;
+}
+
+const countDown = (number) => {
+  while ( number > -1) {
+    console.log(number);
+    number--;
+  }
+}
